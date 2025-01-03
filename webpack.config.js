5ba634e62d1e419336d6d819f5a5ce132e6b2153
lhy1205 <972982286@qq.com>
2025-01-03 09:27:17 +0800

    feat: 新增 webpack 打包功能

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+module.exports = {
+  target: "node", // 指定构建的目标是 Node.js
+  mode: "production", // 指定构建的模式是生产模式
+  entry: {
+    index: "./app.js", // 指定入口文件
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"), // 指定输出的目录
+    filename: "[name].js", // 指定输出的文件名
+    clean: true, // 每次构建之前清空输出目录
+  },
+};
