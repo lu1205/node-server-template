4569ac7ecd1d6312493258d34489899050f4db5c
lhy1205 <972982286@qq.com>
2025-01-20 09:21:51 +0800

    fix: 修复数据库长时间不操作断开连接

--- db/index.js
+++ db/index.js
@@ -4,9 +4,18 @@ const connectionConfig = {
   user: "root",
   password: "Bingtanghulu",
   port: 3306,
-  database: "node_server"
+  database: "node_server",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  // 设置连接超时时间，单位为毫秒
+  connectTimeout: 30000, // 30秒
+  // 设置查询超时时间，单位为毫秒
+  acquireTimeout: 30000, // 30秒
+  // 设置空闲连接超时时间，单位为毫秒
+  idleTimeout: 60000 // 60秒
 }
 
-const sql = await mysql2.createConnection(connectionConfig);
+const sql = await mysql2.createPool(connectionConfig);
 
 export default sql;
